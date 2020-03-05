bf57d933e7c4a41a85f701d7b430d0837bac27e5
aluno <aluno@Lab-15>
2020-03-05 10:31:40 -0300

    Incluindo echos para melhorar o vizual do script.

--- script.sh
+++ script.sh
@@ -4,15 +4,24 @@
 #
 #Autor: Lucas Ferreira Vicente 
 
+
+echo "ENTRANDO NA PASTA DAS FOTOS"
+
 cd imagens-livros
 
+echo "INICIANDO A MODIFICAÇÃO DOS TIPOS E FOTOS"
+
 for imagem in *.jpg
+
 do 
 	echo $imagem
 	convert $imagem $imagem.png
 	img_sem_ext=$(ls $imagem | awk -F. '{print $1}')
 	convert $imagem $img_sem_ext.png
 done 
+
+echo "FINALIZANDO"
+
 cd ..
 
 
